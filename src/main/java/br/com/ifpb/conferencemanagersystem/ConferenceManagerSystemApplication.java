1f33ec0f29b4e75f81c3f5727466c1a5ce249773
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-01-09 17:17:58 -0300

    Initial commit

---
+++ src/main/java/br/com/ifpb/conferencemanagersystem/ConferenceManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.conferencemanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceManagerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpb/conferencemanagersystem/ConferenceManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.conferencemanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
